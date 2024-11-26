d00483ce1d5aff084616604ba30961aeb7e9761e
Cribe Pencheff <cribe@self-made.se>
2024-11-26 19:39:19 +0100

    fix: Update json & styling

--- assets/js/about.js
+++ assets/js/about.js
@@ -28,9 +28,9 @@ async function displayResume() {
       </h3>
       <h4 class="job-project__title">
         <i class="fa-solid fa-building"></i>
-        ${item.homepage ? `
-          <a href="${item.homepage}" target="_blank" rel="noopener noreferrer">${item.workplace}</a>
-          ` : item.workplace}
+        ${item.homepage ?
+          `<a href="${item.homepage}" target="_blank" rel="noopener noreferrer">${item.workplace}</a>`
+          : item.workplace}
       </h4>
       <p class="job-project__location">
         <i class="fa-solid fa-location-dot"></i>
@@ -57,7 +57,9 @@ async function displayResume() {
       </h3>
       <h4 class="education__school">
         <i class="fa-solid fa-building"></i>
-        ${item.workplace}, ${item.location}
+        ${item.homepage ?
+          `<a href="${item.homepage}" target="_blank" rel="noopener noreferrer">${item.workplace}, ${item.location}</a>`
+          : `${item.workplace}, ${item.location}`}
       </h4>
       <p class="education__duration">
         <i class="fa-regular fa-calendar-days"></i>
