a3bac5b1cf0295f76839251cccff302ac41f15bb
Cribe Pencheff <cribe@self-made.se>
2024-11-26 13:34:19 +0100

    add: Links to workplaces + CSS typography layout-shift fix

--- assets/js/about.js
+++ assets/js/about.js
@@ -28,7 +28,9 @@ async function displayResume() {
       </h3>
       <h4 class="job-project__title">
         <i class="fa-solid fa-building"></i>
-        ${item.workplace}
+        ${item.homepage ? `
+          <a href="${item.homepage}" target="_blank" rel="noopener noreferrer">${item.workplace}</a>
+          ` : item.workplace}
       </h4>
       <p class="job-project__location">
         <i class="fa-solid fa-location-dot"></i>
